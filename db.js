2d4a171bbec4b23a584d9d128f899bcebeda7c7d
Miserliness <47208854+Miserliness@users.noreply.github.com>
2021-05-28 21:59:39 +0300

    new

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: 'postgres',
-    password: 'root',
-    host: 'localhost',
-    port: 5432,
-    database: "gps_service_db"
+    user: 'doadmin',
+    password: 'bukclm0kfwy8n7c7',
+    host: 'db-postgresql-nyc3-38342-do-user-9304008-0.b.db.ondigitalocean.com',
+    port: 25060,
+    database: "defaultdb",
+	sslmode = "require"
 })
 
 module.exports = pool
