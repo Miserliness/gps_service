ef76271a66019f7a092d73f7abbc01dc785a9dc7
Miserliness <thebeatbox@gmail.com>
2021-06-01 23:39:36 +0000

    cors

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const userRouter = require('./routes/user.routes')
 const boxRouter = require('./routes/box.routes')
 const path = require('path')
 const coordinatesRouter = require('./routes/coordinates.routes')
-const PORT = process.env.PORT || 8080
 
 const app = express()
+const cors = require('cors')
+
 app.use(express.json())
+
+app.use(cors());
+app.options('*', cors());
+
+app.set('port', process.env.PORT || 8080)
+
+
 app.use('/api', userRouter)
 app.use('/api', boxRouter)
 app.use('/api', coordinatesRouter)
@@ -14,10 +22,11 @@ app.use('/api', coordinatesRouter)
 app.use(express.static(path.resolve(__dirname, 'client', 'dist'))) // подключаем папки dist
 app.use(express.static(path.resolve(__dirname, 'client', 'public'))) // и папку public
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-})
+    })
 
-
-
-app.listen(PORT, console.log('server started on port ${PORT}'))
+app.listen(app.get('port'), function() { 
+    console.log('we are listening on: ', 
+    app.get('port'))
+});
